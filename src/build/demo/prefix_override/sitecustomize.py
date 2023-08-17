5001d95ce6d7ff3cb1c7d7e36e3a6b7a05011b01
Jetson Nano 2GB 1 <info@monashcav.com>
2023-08-17 19:51:54 +1000

    Added rc_car_node package

---
+++ src/build/demo/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/mcav-cv2x-2/mrn/src/install/demo'
---
+++ src/build/demo/share/demo/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/mcav-cv2x-2/mrn/src/build/demo"
